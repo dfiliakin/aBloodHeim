ac5e36e1faf9df39e3e24c0bf00991a52abbb2b0
Denis Filiakin <dfilyakin@nymbus.com>
2024-10-24 18:47:19 +0300

    better logging

--- game/game_state.py
+++ game/game_state.py
@@ -1,5 +1,7 @@
+import logging
 from abc import ABC, abstractmethod
-from scenes import MainMenuScene, LocustGuildScene
+
+from scenes import LocustGuildScene, MainMenuScene
 from toolbox.scene import Status as SceneStatus
 
 
@@ -12,19 +14,19 @@ class GameState(ABC):
         return self.scene.run()
 
     @abstractmethod
-    def main_menu(self):
+    def main_menu(self, screen):
         pass
 
     @abstractmethod
-    def locust_guild(self):
+    def locust_guild(self, screen):
         pass
 
     @abstractmethod
-    def bloodheim(self):
+    def bloodheim(self, screen):
         pass
 
     @abstractmethod
-    def fight(self, hero, enemies):
+    def fight(self, hero, enemies, screen):
         pass
 
 
@@ -35,18 +37,18 @@ class MainMenuState(GameState):
         self.scene = MainMenuScene(self.screen)
 
     def main_menu(self, screen):
-        print("Already in the main menu.")
+        logging.info("Already in the main menu.")
 
     def locust_guild(self, screen):
-        print("Loading the 'Locust Guild' state.")
+        logging.info("Loading the 'Locust Guild' state.")
         return LocustGuildState(screen)
 
     def bloodheim(self, screen):
-        print("Loading the 'Bloodheim' state.")
+        logging.info("Loading the 'Bloodheim' state.")
         return BloodheimState(screen)
 
     def fight(self, hero, enemies, screen):
-        print("Cannot get into the fight form the menu.")
+        logging.info("Cannot get into the fight form the menu.")
 
 
 class LocustGuildState(GameState):
@@ -56,18 +58,18 @@ class LocustGuildState(GameState):
         self.scene = LocustGuildScene(self.screen)
 
     def main_menu(self, screen):
-        print("Loading the main menu.")
+        logging.info("Loading the main menu.")
         return MainMenuState()
 
     def locust_guild(self, screen):
-        print("Already in the locust guild.")
+        logging.info("Already in the locust guild.")
 
     def bloodheim(self, screen):
-        print("Loading the 'Bloodheim' state.")
+        logging.info("Loading the 'Bloodheim' state.")
         return BloodheimState()
 
     def fight(self, hero, enemies, screen):
-        print("Cannot get into the fight directly from the locust guild.")
+        logging.info("Cannot get into the fight directly from the locust guild.")
 
 
 class BloodheimState(GameState):
@@ -77,20 +79,20 @@ class BloodheimState(GameState):
         self.scene = None
 
     def main_menu(self, screen):
-        print("Loading the main menu.")
+        logging.info("Loading the main menu.")
         return MainMenuState(screen)
 
     def locust_guild(self, screen):
-        print("Loading the 'Locust Guild' state.")
+        logging.info("Loading the 'Locust Guild' state.")
         return LocustGuildState(screen)
 
     def bloodheim(self, screen):
-        print("Already in the bloodheim.")
+        logging.info("Already in the bloodheim.")
 
     def fight(self, hero, enemies, screen):
-        print("Starting the fight...")
-        print(f"    - Hero: {hero}")
-        print(f"    - Enemies: {enemies}")
+        logging.info("Starting the fight...")
+        logging.info(f"    - Hero: {hero}")
+        logging.info(f"    - Enemies: {enemies}")
         FightState(hero, enemies, screen)
 
 
@@ -104,14 +106,14 @@ class FightState(GameState):
         self.enemies = enemies
 
     def main_menu(self, screen):
-        print("Cannot leave the fight.")
+        logging.info("Cannot leave the fight.")
 
     def locust_guild(self, screen):
-        print("Cannot leave the fight.")
+        logging.info("Cannot leave the fight.")
 
     def bloodheim(self, screen):
-        print("The fight is over. Getting back to the Bloodheim.")
+        logging.info("The fight is over. Getting back to the Bloodheim.")
         BloodheimState()
 
     def fight(self, hero, enemies, screen):
-        print("Already in the fight.")
+        logging.info("Already in the fight.")
